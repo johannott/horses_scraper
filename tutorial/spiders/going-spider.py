12ab99d7d9e161d8b62c162224793cb9f01703a6
Johann Ott <johann.ott@gmail.com>
2020-02-25 16:53:08 +0000

    First commit to horses scraper

---
+++ tutorial/spiders/going-spider.py
@@ -0,0 +1,22 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from tutorial.items import GoingItem
+
+
+class GoingSpider(scrapy.Spider):
+    name = "going"
+
+    start_urls = [
+        'http://maps.turftrax.co.uk/latestgoingreport.asp?course=Cheltenham'
+    ]
+
+    def parse(self, response):
+        
+        going = response.css('td.mapinfoheader div::text')[2].get()
+        loader = ItemLoader(item=GoingItem(), response=response)
+        loader.add_value('going', going)
+        yield loader.load_item()
+
+        # yield {
+        #     'going': response.css('td.mapinfoheader div::text')[2].get()
+        # }
